f2a330a6b2b3c9636480054f318ac957929a0f7c
Javier Montiel Lopez <jmonlop0804@g.educaand.es>
2024-11-26 10:37:20 +0100

    Mod app.java gen30 num

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
 
     public static void mostrarNumerosAleatorios() {
         Random rand = new Random();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 30; i++) {
             System.out.println(rand.nextInt(10) + 1);
         }
     }
